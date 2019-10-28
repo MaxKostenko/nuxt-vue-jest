dea2d69e1df1e9adb2c6683eee13bf70714cd8d9
Maxim <m.u.kostenko@gmail.com>
2019-10-28 09:20:34 +0100

    Vue-jest. Example of broken import

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  globals: {
+    /**/
+    'vue-jest': {
+      resources: {
+        scss: [
+          './assets/styles.scss',
+        ]
+      }
+    }
+
+    /**/
+  },
+};
